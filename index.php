67a5aed5ecf2d383bf9216a84c92d80865fc286d
Alan Christian Gonçalves Ferreira <alanchristiangoncalvesferreira@hotmail.com>
2021-07-03 17:57:29 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,143 @@
+<html lang="pt-br">
+<head>
+<title>Alan Christian / Archangel Four</title>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet"  href="style.css" />
+<script type="text/javascript" src="script.js"></script>
+  
+</head>
+
+<body>
+<header>
+    <nav>
+        <a id="menu" href="#"  onclick="menuaparecer()"> 
+            <img class="logo" src="image/Logo-menu-.png" alt="Menu logo Alan Christian " onclick="menuaparecer()" /> <p id="menuhamburguer">☰</P> </a>
+    </nav>
+    <div id="menuverti" class="menu-bar">
+        <ul>
+            <li><a href="https://www.behance.net/alanchristiangf" alt="Behance" target="_blank" ><img src="image/behance.png" alt="Behance" style="width: 70px;
+    height: 55px;"></a></li>
+            <li><a href="https://www.artstation.com/alanchristiangoncalvesferreira" alt="Artstation" target="_blank"><img src="image/artstation.png" alt="Artstation"></a></li>
+            <li><a href="https://github.com/AlanChristianGoncalvesFerreira" alt="Github" target="_blank"><img src="image/github.png" alt="Github"></a></li>
+            <li><a href="https://br.pinterest.com/AlanChristianGoncalvesFerreira/" alt="Pinterest" target="_blank"><img src="image/pinterest.png" alt="Pinterest"></a></li>
+            <li><a href="https://www.facebook.com/DesignerAlanChristian" alt="Facebook" target="_blank"><img src="image/facebook.png" alt="Facebook"></a></li>
+            <li><a href="https://www.instagram.com/alanchristiandesign/" alt="Instagram" target="_blank"><img src="image/instagram.png" alt="Instagram"></a></li>
+            <li><a href="https://alanchristiangoncalvesferreira.blogspot.com/" alt="Blogger" target="_blank"><img src="image/blogger.png" alt="Blogger"></a></li>
+        </ul>
+
+
+    </div>
+    
+</header>
+<div id="slide-one">
+
+</div>
+<div id="container_sobremim">
+    
+    <div id="figure">
+    <img src="image/alanchristian.png" alt="Alan Christian"><br>
+    <button type="button">Currículo</button>    
+</div>
+    <div id="article">
+     <h1>Sobre mim </h1>
+     <p>Designer Alan Christian Gonçalves Ferreira , 
+desde 2012 atuando no mercado para pequenas , 
+médias e grandes empresas .</p>
+    <h1>Ranking</h1> 
+    <p>projetos : 280 <br>
+Facebook : 4324 seguidores <br> 
+blog : 13 mil visualizações<br>
+prêmiações : melhor projeto photoshop saga school </p>   
+</div>
+    
+</div>
+<div id="container_servicos">
+    <h1>Nossos serviços</h1>
+  <div id="listas_servicos">
+      
+
+    <ul>
+        <li><img src="image/icone-identidade-visual.png" alt="Identidade visual">   
+            <P>Identidade visual</br>
+               <span>LOGOTIPO , TIPOGRAFIAS , MANUAL 
+DE IDENTIDADE VISUAL </span> 
+        </P></li>
+        <li>
+            <img src="image/icone-design-grafico.png" alt="Design Gráfico" >
+            <p>Design Gráfico</br>
+            <span>CARTÕES , FLYERS , BANNERS , 
+LAYOUTS , IMPRESSOS EM GERAL </span>
+            </p>
+
+
+        </li>
+
+    </ul>
+    <ul>
+        <li><img src="image/icone-video.png" alt="Edição de vídeo">
+            <p> Edição de vídeo </br>
+                <span>cortes , aúdio , vinhetas </span>
+
+            </p>
+    
+    </li>
+        <li>
+            <img src="image/icone-social-midia.png" alt="Social Mídia">
+            <p> Social Mídia </br>
+                <span>Arte para postes , stores </span>
+
+            </p></li>
+
+    </ul>
+    <ul>
+        <li>
+        <img src="image/icone-3d.png" alt="3D">
+            <p>Modelagem 3D </br> 
+                <span>PERSONAGENS , CÉNÁRIOS , ASSETS </span>
+
+            </p>
+
+        </li>
+        <li>
+        <img src="image/icone-code.png" alt="Desenvolvimento" style="width:96px; height:30px;
+    margin: 32px 0 0 0;">
+            <p> Sites UI/UX </br>
+                <span>HOTSITES , LOJA VIRTUAL , UX DESIGN </span>
+
+            </p>
+
+        </li>
+
+    </ul>
+</div>
+</div>
+
+<div id="container_cliente">
+        <h1> Clientes</h1>
+ <ul>
+    <li class="onofre"></li>
+    <li class="nutrisami"></li>
+    <li class="agenciachoque"></li>
+    <li class="jonathananjos"></li>
+    <li class="tadellasfood"></li>
+    
+ </ul>
+
+</div>
+
+<div id="container_contato">
+        <h1>Contato</h1>
+    
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScT1gxkBqQJTzlxhO1pJE2UPjtTJVJQ4i-ZD27DUbixF5bkMw/viewform?embedded=true" width="640" height="724" frameborder="0" marginheight="0" marginwidth="0" id="formulario" >Carregando…</iframe>
+        
+        
+        <p>Localização São Paulo , Brasil . 
+Atendemos clientes no mundo todo .</br>
+Para um orçamento , entre em contato
+através desse formulário .</p>
+</div>
+
+</body>
+</html>
